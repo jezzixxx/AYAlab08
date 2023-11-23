de3a22ef689489a22ea765ea9d62a82861f382c3
jezzixxx <112870068+jezzixxx@users.noreply.github.com>
2023-11-23 17:51:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using MyLib;
+using System.Reflection;
+using System.Xml.Linq;
+internal class Task
+{
+    private static void Main()
+    {
+        var mylib = Assembly.LoadFrom("C:\\Users\\pkapa\\source\\repos\\lab07\\MyLib.dll");
+        var types = mylib.GetExportedTypes();
+        XDocument xdoc = new XDocument();
+        uint num;
+        XElement Classes = new XElement("classes");
+        xdoc.Add(Classes);
+        foreach (var type in types) {
+            XElement el = new XElement("class");
+            XAttribute el_attr = new XAttribute("name", type.FullName);
+            el.Add(el_attr);
+            XElement Properties = new XElement("properties");
+            el.Add(Properties);
+            XElement Methods = new XElement("methods");
+            el.Add(Methods);
+            num = 1;
+            foreach (var method in type.GetMethods()) { Methods.Add(new XElement($"method{num}",method)); ++num; }
+            num = 1;
+            foreach (var prop in type.GetProperties()) { Properties.Add(new XElement($"property{num}", prop)); ++num; }
+            Classes.Add(el);
+        }
+        
+        xdoc.Save("classes.xml");
+    }
+}
